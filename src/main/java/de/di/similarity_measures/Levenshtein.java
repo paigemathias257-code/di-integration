1a7c95978508715cc89ad62d4902a5da0a1069b6
paigemathias257-code <paigemathias257@gmail.com>
2026-05-29 20:30:08 +0200

    Finished Levenshtein- All tests pass

--- src/main/java/de/di/similarity_measures/Levenshtein.java
+++ src/main/java/de/di/similarity_measures/Levenshtein.java
@@ -39,9 +39,50 @@ public class Levenshtein implements SimilarityMeasure {
         // algorithm. Depending on whether the inner flag withDamerau is set, the Damerau extension rule should be    //
         // used during calculation or not. Hint: Implement the Levenshtein algorithm here first, then copy the code   //
         // to the String tuple function and adjust it a bit to work on the arrays - the algorithm is the same.        //
+        // dealing with silly circumstances
+        if (string1.isEmpty() && string2.isEmpty()) {
+            levenshteinSimilarity = 1;
+        }
+        else if (string1.isEmpty() || string2.isEmpty()) {
+            levenshteinSimilarity = 0;
 
+        }else {
+            if (withDamerau) {
+                for (int i = 1; i <= string2.length(); i++) {
+                    lowerLine[0] = i;
+                    for (int j = 1; j <= string1.length(); j++) {
+                        if (string1.charAt(j - 1) == (string2.charAt(i - 1))) {
+                            lowerLine[j] = upperLine[j - 1];
+                        } else {
+                            // avoid using upperupperLine when not applicable
+                            if (j >= 2 && i >= 2 && (string1.charAt(j - 1) == (string2.charAt(i - 2)) && (string1.charAt(j - 2) == (string2.charAt(i - 1))))) {
+                                lowerLine[j] = 1 + Math.min(Math.min(upperLine[j], upperLine[j - 1]), Math.min(lowerLine[j - 1], upperupperLine[j - 2]));
+                            } else {
+                                lowerLine[j] = 1 + Math.min(Math.min(upperLine[j], upperLine[j - 1]), lowerLine[j - 1]);
+                            }
 
-
+                        }
+                    }
+                    upperupperLine = Arrays.copyOf(upperLine, upperLine.length);
+                    upperLine = Arrays.copyOf(lowerLine, lowerLine.length);
+                }
+                levenshteinSimilarity = 1 - (double) lowerLine[lowerLine.length - 1] / Math.max(string1.length(), string2.length());
+            } else {
+                for (int i = 1; i <= string2.length(); i++) {
+                    lowerLine[0] = i;
+                    for (int j = 1; j <= string1.length(); j++) {
+                        if (string1.charAt(j - 1) == (string2.charAt(i - 1))) {
+                            lowerLine[j] = upperLine[j - 1];
+                        } else {
+                            int temp = Math.min(Math.min(upperLine[j], upperLine[j - 1]), lowerLine[j - 1]);
+                            lowerLine[j] = temp + 1;
+                        }
+                    }
+                    upperLine = Arrays.copyOf(lowerLine, lowerLine.length);
+                }
+                levenshteinSimilarity = 1 - (double) lowerLine[lowerLine.length - 1] / Math.max(string1.length(), string2.length());
+            }
+        }
         //                                                                                                            //
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -75,9 +116,45 @@ public class Levenshtein implements SimilarityMeasure {
         // algorithm. Depending on whether the inner flag withDamerau is set, the Damerau extension rule should be    //
         // used during calculation or not. Hint: Implement the Levenshtein algorithm above first, then copy the code  //
         // to this function and adjust it a bit to work on the arrays - the algorithm is the same.                    //
-
-
-
+        if (withDamerau){   // calculation with Damerau-Levenshtein distance
+            for (int i = 1; i <= strings1.length; i++) {        // iterate down through letters of first string array
+                lowerLine[0] = i;                               // set first digit of lowerLine to pass #
+                for (int j = 1; j <= strings2.length; j++) {    // iterate over through letter of second string
+                    // choose diagonal value when letters match
+                    if (strings1[i - 1].equals(strings2[j - 1])) {
+                        lowerLine[j] = upperLine[j - 1];
+                    // choose action when letters do not match
+                    } else {
+                        // avoid using upperupperLine when not applicable
+                        if (j>=2 && i>= 2 && strings1[i - 1].equals(strings2[j - 2]) && strings1[i - 2].equals(strings2[j - 1])){
+                            lowerLine[j] = 1 + Math.min(Math.min(upperLine[j], upperLine[j - 1]), Math.min(lowerLine[j - 1], upperupperLine[j - 2]));
+                        } else {
+                            lowerLine[j] = 1 + Math.min(Math.min(upperLine[j],upperLine[j-1]),lowerLine[j-1]);}
+                    }
+                }
+                upperupperLine = Arrays.copyOf(upperLine, upperLine.length);    // move upperLine up
+                upperLine = Arrays.copyOf(lowerLine, lowerLine.length);         // move lowerLine up
+            }
+            // calculate Levenshtein Similarity
+            levenshteinSimilarity = 1 - (double) lowerLine[lowerLine.length - 1]/Math.max(strings1.length,strings2.length);
+        }
+        else {  // calculation without Damerau-Levenshtein distance
+            for (int i = 1; i <= strings1.length; i++) {    // iterate down through letters of first string array
+                lowerLine[0] = i;                           // set first digit of lowerLine to pass #
+                for (int j = 1; j <= strings2.length; j++){ // iterate over through letter of second string
+                    // choose diagonal value when letters match
+                    if (strings1[i - 1].equals(strings2[j - 1])) {
+                        lowerLine[j] = upperLine[j - 1];
+                    // chose action when letters do not match
+                    } else {
+                        lowerLine[j] = 1 + Math.min(Math.min(upperLine[j], upperLine[j - 1]), lowerLine[j - 1]);
+                    }
+                }
+                upperLine = Arrays.copyOf(lowerLine, lowerLine.length); // move lines up
+            }
+            // calculate Levenshtein Similarity
+            levenshteinSimilarity = 1 - (double) lowerLine[lowerLine.length - 1]/Math.max(strings1.length,strings2.length);
+        }
         //                                                                                                            //
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
