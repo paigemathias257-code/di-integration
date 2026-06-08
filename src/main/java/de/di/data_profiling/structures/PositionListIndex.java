6bd2205c0507764abe7be4f1c145cb07a21a08dc
paigemathias257-code <paigemathias257@gmail.com>
2026-06-08 17:51:58 +0200

    PLI works for all but one test (the last assertTrue statement)

--- src/main/java/de/di/data_profiling/structures/PositionListIndex.java
+++ src/main/java/de/di/data_profiling/structures/PositionListIndex.java
@@ -68,55 +68,36 @@ public class PositionListIndex {
         // Calculate the intersection of one PLI's clusters and another PLI's (conveniently already inverted)         //
         // invertedClusters. The clustersIntersection is a new list that stores the intersection result. Note that    //
         // the clusters are "Stripped Partitions", which means that only clusters of size >1 are part of the result.  //
-        String[] temp = new String[invertedClusters.length];
+
+        String[] combo = new String[invertedClusters.length];
         for (int row = 0; row<invertedClusters.length;row++){
             for (int oci = 0; oci<clusters.size();oci++) { // outer cluster index
                 for (int ici = 0; ici < clusters.get(oci).size(); ici++) { // inner cluster index
                     if (clusters.get(oci).get(ici).equals(row)){ //gets the probe value for a row
-                        temp[row] = "("+clusters.indexOf(clusters.get(oci))+","+invertedClusters[row]+")"; // combines the values of row into a tuple
+                        combo[row] = "("+clusters.indexOf(clusters.get(oci))+","+invertedClusters[row]+")"; // combines the values of row into a tuple
                     }
                 }
             }
         }
+        // creates a set of the possible combinations to be compares
+        Set<String> possibilities = new LinkedHashSet<>(Arrays.asList(combo));
+        String[] compare = possibilities.toArray(new String[0]);
+        for (int i = 0; i<possibilities.size();i++) {
+            IntArrayList temp = new IntArrayList();
+            for (int j = 0; j < combo.length; j++) {
+                if (compare[i] == null) {
+                    break;
 
-
-        /*
-        List<String> temp2 =  Arrays.asList(temp);
-        for (int i = 0; i<temp2.size();i++){
-            if (temp2.get(i)== null){
-                temp2.remove(temp2.get(i));
-            }
-        }
-
-        List<String> temp2 =  Arrays.asList(temp);
-        for (int tempIndex = 0; tempIndex < temp2.size()-1; tempIndex++){
-            ArrayList<Integer> matches = new ArrayList<>();
-            for (int compareIndex = tempIndex+1; compareIndex<)
-            if (temp2.get(tempIndex).equals(temp2.get(compareIndex))){
-                // add index to list, then delete tuple
-                matches.add(tempIndex);
-                temp2.remove(compareIndex);
-            }
-        }
-        */
-        /*
-        for (int outerClusterIndex=0; outerClusterIndex<clusters.size();outerClusterIndex++) {  // iterates through probe clusters
-            int[] temp = new int[clusters.get(outerClusterIndex).size()];
-            IntArrayList temp2 = new IntArrayList();
-            for (int innerClusterIndex=0; innerClusterIndex<clusters.get(outerClusterIndex).size();innerClusterIndex++) {    // iterates within cluster
-                temp[innerClusterIndex] = clusters.get(outerClusterIndex).get(innerClusterIndex);
-            }
-            for (int tempIndex = 0; tempIndex<temp.length; tempIndex++) {
-                for (int buildIndex = 0; buildIndex < invertedClusters.length; buildIndex++) { // iterates through build
-                    if (temp[tempIndex] ==  buildIndex){
-                        temp2.add(temp[tempIndex]);
+                } else if (combo[j] != null){
+                    if (compare[i].equals(combo[j])) {
+                        temp.add(j);
                     }
                 }
             }
-                clustersIntersection.add(temp2);
+            if (temp.size() > 1) {
+                clustersIntersection.add(temp);
+            }
         }
-        */
-
 
         //                                                                                                            //
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
